9489cd8c70d0d1d0b671291c0aed73699c4382b8
Unknown <chikimoco@gmail.com>
2017-09-18 10:44:34 -0500

    Documentacion

--- ProjectoFecha/src/projectofecha/Bank.java
+++ ProjectoFecha/src/projectofecha/Bank.java
@@ -1,23 +1,34 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projectofecha;
 
 /**
- *
- * @author edgar.cambranes
+ * Class Bank Creates new types of Banks
+ * @author  edgar.cambranes
  */
 public class Bank{
-    private Customer[] clients;
-    private int numberOfCustomers;
+    //Attributes of the class
+    private Customer[] clients;//Vector of Custumers
+    private int numberOfCustomers;//Number of Customers in the bank
     
+
+    /**
+    * Constructor Bank
+    * Set the number max of clients
+    * Initialize clients with the valor of max clients
+    * Initialize number of clients in zero
+    * @param maxClients Number max of clients
+    * 
+    */
     public Bank(int maxClients){
         clients = new Customer[maxClients];
         numberOfCustomers = 0;
     }
     
+    /**
+     * Method addCustumer
+     * Add a custumer to the vector of custumers in the current bank
+     * @param client Custumer to add to the current bank
+     * @return boolean return true if the custumer was added sucessfully
+    */
     public boolean addCustomer(Customer client){
         boolean flag = false;
         if(numberOfCustomers < clients.length ){
@@ -28,6 +39,12 @@ public class Bank{
         return flag;
     }
     
+    /**
+     * Method removeCustomer
+     * remove a custumer searching his unique id in the vector of clients
+     * @param IDcustomer Unique id of customer 
+     * @return false if there is not any Customer with that Id
+     */
     public boolean removeCustomer(int IDcustomer){
         boolean flag = false;
             for(int i = 0; i < numberOfCustomers; i++){
@@ -42,6 +59,14 @@ public class Bank{
         return flag;
     }
     
+    /**
+     * Method editCustomer 
+     * Edit a Customer searching his Unique Id and Editing His Name an Last Name
+     * @param IDcustomer Unique ID of Customer
+     * @param firstName Name to edit
+     * @param lastName Last Name to edit
+     * @return false if there is not a Customer with the specified Id
+     */
     public boolean editCustomer(int IDcustomer,String firstName, String lastName){
         boolean flag = false;
             for(int i = 0; i < numberOfCustomers; i++){
@@ -54,6 +79,13 @@ public class Bank{
         return flag;
     }
     
+    /**
+     * Method searchClient 
+     * Make a Search of a Client with the Unique Id of a Customer
+     * if the Client is finded the method will return the Custumer object
+     * @param IDClient Id to search
+     * @return null if the client was not finded
+     */
     public Customer searchClient(int IDClient){
         Customer temp = null;
         for(Customer client: clients){
@@ -64,6 +96,11 @@ public class Bank{
         return temp;
     }
     
+    /**
+     * Method toString
+     * Return a String representation of each clients and his Accounts
+     * @return String, List of Clients in the bank and their info
+     */
     public String toString(){
       String output= "";
       for(int i = 0; i < numberOfCustomers; i++){
@@ -71,7 +108,5 @@ public class Bank{
       }
       return output;
     }
-    
-    
-    
-}
+     
+}//End of class
--- ProjectoFecha/src/projectofecha/Customer.java
+++ ProjectoFecha/src/projectofecha/Customer.java
@@ -1,23 +1,27 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package projectofecha;
 
 /**
- *
+ * Class Customer
+ * Creates Clients that can be added to banks
  * @author edgar.cambranes
  */
 public class Customer{
-    private static int IDkey;
-    private int IDCustomer;
-    private String firstName;
-    private String lastName;
-    private Account accounts[];
-    private int numberOfAccounts;
+    //Attributes of the class
+    private static int IDkey;//Global variable for new ID´s
+    private int IDCustomer;//Id of the Customer
+    private String firstName;//Name of Customer
+    private String lastName;//Last name of Customer
+    private Account accounts[];//Vector of accounts associated with the client
     
-       
+    private int numberOfAccounts;//Current number of accounts
+    
+    /**
+     * Constructor ONE Customer 
+     * Sets the Id of the new Customer taking the Global Variable IDKey and
+     * increases this by one
+     * @param firstName sets the name of the client
+     * @param lastName sets the lastname of the client
+     */   
     public Customer (String firstName, String lastName) {
         this.IDCustomer =IDkey;
         IDkey++;
@@ -25,6 +29,14 @@ public class Customer{
         this.lastName = lastName;
     }
     
+    /**
+     * Constructor TWO Customer 
+     * Sets the Id of the new Customer taking the Global Variable IDKey and
+     * increases this by one
+     * @param firstName sets the name of the client
+     * @param lastName sets the lastname of the client
+     * @param maxAccounts sets the maximum number of accounts for the client
+     */  
     public Customer (String firstName, String lastName, int maxAccounts) {
         this.IDCustomer =IDkey;
         IDkey++;
@@ -32,29 +44,61 @@ public class Customer{
         this.lastName = lastName;
         this.accounts = new Account[maxAccounts];
     }
+    
+    /**
+     * Gets the Id of current Customer
+     * @return id
+     */
     public int getIDCustomer(){
         return IDCustomer;
     }
     
+    /**
+     * Gets the number of Accounts that have the client
+     * @return int number of accounts
+     */
     public int getNumberOfAccounts(){
         return numberOfAccounts;
     }
     
+    /**
+     * Set the name of the client
+     * @param firstName name of client
+     */
     public void setFirstName(String firstName){
         this.firstName = firstName;
     }
     
+    /**
+     * Gets the name of client
+     * @return String name
+     */
     public String getFirstName(){
         return this.firstName;
     }
     
+    /**
+     * Set the last name of the client
+     * @param lastName last name of client
+     */
     public void setLastName(String lastName){
         this.lastName = lastName;
     }
     
+    /**
+     * Gets the last name of client
+     * @return String last name
+     */
     public String getLastName(){
         return this.lastName;
     }
+    
+    /**
+     * Method to add a new account to the vector of accounts in the current cutomer
+     * increases in one the number of accounts
+     * @param account account to add to the current customer
+     * @return boolean return true if the account was added sucessfully
+     */
     public boolean addAccount(Account account){
         boolean flag = false;
         if(numberOfAccounts < accounts.length ){
@@ -65,6 +109,12 @@ public class Customer{
         return flag;
     }
     
+    /**
+     * Method removeAccount
+     * remove a account searching his unique id in the vector of accounts
+     * @param IDAccount Unique id of account
+     * @return false if there is not any account with that id
+     */
     public boolean removeAccount(int IDAccount){
         boolean flag = false;
             for(int i = 0; i < numberOfAccounts; i++){
@@ -79,6 +129,13 @@ public class Customer{
         return flag;
     }
     
+    /**
+     * Method searchAccount
+     * Make a Search of an Account with the Unique Id of the account
+     * if the account is finded the method will return the Account object
+     * @param IDAccount Id to search
+     * @return null if the Accoubnt was not finded
+     */
     public Account searchAccount(int IDAccount){
         Account temp = null;
         if(accounts != null){
@@ -91,14 +148,30 @@ public class Customer{
         return temp;
     }
     
+    /**
+     * Gets an Account if the Id Exist
+     * @return id Account
+     * @param IDAccount id of account
+     */
     public Account getAccount(int IDAccount){                
         return searchAccount(IDAccount);
     }
     
+    /**
+     * Gets all the Accounts what does the client have
+     * @return id Accounts Vector
+     */
     public Account[] getAccounts(){
         return accounts;
     }
     
+    /**
+     * Method to edit the Account 
+     * Search the id account in the vector of accounts and modify the balance
+     * @param IDAccount Id to search
+     * @param balance Balance to modify
+     * @return true if the id was finded
+     */
     public boolean editAccount(int IDAccount,int balance){
         boolean flag = false;
             for(int i = 0; i < numberOfAccounts; i++){
@@ -111,7 +184,11 @@ public class Customer{
         return flag;
     }
     
-    
+    /**
+     * Method to know if snd object is a Customer and have the same id 
+     * @param obj object customer to compare
+     * @return true if the object is the same
+     */
     @Override
     public boolean equals(Object obj){
         boolean flag = false;
@@ -125,6 +202,11 @@ public class Customer{
         return flag;
     }
     
+    /**
+     * Method that returns a String with the info of the current Customer
+     * and all his accounts 
+     * @return String with the info
+     */
     public String toString(){
         String output = "ID Customer: " + this.IDCustomer+
                " ,Fist Name: " + this.firstName+
@@ -137,6 +219,13 @@ public class Customer{
         return output;
     }
     
+    /**
+     * Method to transfer money from a debit card to another one from the same client
+     * @param IDSender id of the account tha will pay
+     * @param IDReciever id of the account tha will recive
+     * @param ammount ammoubnt to give
+     * @return true if the transaction was successful
+     */
     public boolean myTransferDD(int IDSender, int IDReciever, double ammount){
         Account sender = null;
         Account reciever = null;
@@ -163,6 +252,14 @@ public class Customer{
         }
         return flag;
 }
+    
+    /**
+     * Method to transfer money from a debit card to a credit one from the same client
+     * @param IDSender id of the account tha will pay
+     * @param IDReciever id of the account tha will recive
+     * @param ammount ammount to give
+     * @return true if the transaction was successful
+     */
     public boolean myTransferDC(int IDSender, int IDReciever, double ammount){
         Account sender = null;
         Account reciever = null;
@@ -191,6 +288,14 @@ public class Customer{
 
     }
     
+    /**
+     * Method to transfer money from a debit card to a debit one from another client
+     * @param IDSender id of the account tha will pay
+     * @param IDReciever id of the account tha will recive
+     * @param rCustomer Custumer that will recive the money
+     * @param ammount ammount to give
+     * @return true if the transaction was successful
+     */
     public boolean thirdTransferDD(int IDSender, Customer rCustomer, int IDReciever, double ammount){
         boolean flag = false;
         Account sender = null;
@@ -222,5 +327,5 @@ public class Customer{
         return flag;
     }
 
-}                               
+}//End of class                               
 
