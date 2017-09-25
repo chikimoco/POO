a8b8016304782bcecb7c14ddb2bd6bde5d0774e2
Hugo Pérez <hugoa.perezh@gmail.com>
2017-09-25 12:28:13 -0500

    Nuevas clases Debit y Credit

---
+++ ProjectoFecha/src/projectofecha/Credit.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package projectofecha;
+
+/**
+ *
+ * @author hugop
+ */
+public class Credit extends Account {
+    
+    public Credit(String typeAccount, double balance) {
+        super(typeAccount, balance);
+    }
+ 
+}
